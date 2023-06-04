87551811633bd0c623fa9b15a32c814ddbe38b0e
ejarmand <earmand@ucsd.edu>
2023-06-03 18:40:51 -0700

    update diff exp

--- get_diffexp_genes.R
+++ get_diffexp_genes.R
@@ -1,16 +1,18 @@
-setwd('/Users/alexanderjambor/Desktop/UCSD/SP23/BENG203/GroupProject/BRCAClassifier/')
+# setwd('/Users/alexanderjambor/Desktop/UCSD/SP23/BENG203/GroupProject/BRCAClassifier/')
+setwd('/home/ethan/homework/cse283/BRCAClassifier/')
 
 library(Seurat)
 library(dplyr)
 library(edgeR)
 library(Matrix)
 
-normal = read.table('data/GSE131512_normalTPM.txt')
+# there's not really a reason to read this using seurat, but it's fine
+normal = read.table('data/initial/GSE131512_normalTPM.txt')
 normal_seurat = CreateSeuratObject(counts = normal)
 normal_seurat$group = "Normal"
 rm(normal)
 
-brca = read.table('data/GSE131512_cancerTPM.txt')
+brca = read.table('data/initial/GSE131512_cancerTPM.txt')
 brca_seurat = CreateSeuratObject(counts = brca)
 brca_seurat$group = "BRCA"
 rm(brca)
@@ -18,21 +20,43 @@ rm(brca)
 all_seurat = merge(normal_seurat, brca_seurat)
 rm(normal_seurat)
 rm(brca_seurat)
- 
-dge = DGEList(counts = all_seurat@assays$RNA@counts, group = all_seurat$group)
 
-# dge = calcNormFactors(dge)
-# dge = dge[filterByExpr(dge), keep.lib.sizes = FALSE]
+counts = all_seurat@assays$RNA@counts
+
+# probably unesesary 
+use_genes = rowSums(is.na(counts)) == 0
+counts = counts[use_genes, ]
+
+# can't filter by expr w/o norm factors
+# instead set a minimum threshold
+
+min_thresh = 2 # require tpm minimum
+n_sample_thresh = 3 # require threshold reached in n samples
+
+use_genes2 = rowSums(counts>=min_thresh) >= n_sample_thresh
+counts = counts[use_genes2, ]
+print(dim(counts))
+
+dge = DGEList(counts = counts, group = all_seurat$group)
+
+# keep = filterByExpr(dge)
+# dge = dge[keep, ,keep.lib.sizes=FALSE]
+
+print(dim(dge))
 
 des = model.matrix(~ all_seurat$group)
-dge = estimateDisp(dge, des)
+dge = estimateDisp(dge, des, , trend.method='locfit')
 fit = glmQLFit(dge, des)
 glm_res = glmQLFTest(fit)
 
-adj_pvals = p.adjust(glm_res$table$PValue, method = "BH")
+adj_pvals = p.adjust(glm_res$table$PValue, method = "BY")
 fdr_threshold = 0.05
+lfc_threshold = 1
 
-degs = glm_res$table[(adj_pvals <= fdr_threshold) & (abs(glm_res$table$logFC) >= 1), ]
+# print(head(glm_res$table))
+
+degs = glm_res$table[((adj_pvals <= fdr_threshold) & (abs(glm_res$table$logFC) >= lfc_threshold)), ]
 ensg_ids = data.frame(rownames(degs))
 colnames(ensg_ids) = NULL
+print(dim(ensg_ids))
 write.csv(ensg_ids, 'features/diff_exp_genes.csv', row.names = FALSE)
