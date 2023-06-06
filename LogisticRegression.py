7c9587c3e876ab9d6276f2c89833a5e82e9229e6
Alex Jambor <ajambor@ucsd.edu>
2023-06-06 13:17:42 -0700

    wip

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,7 +1,78 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_curve, precision_recall_curve, accuracy_score, f1_score, auc
+import matplotlib.pyplot as plt
 
 import numpy as np
+import os
 
-x = np.loadtxt('./data/processed/X_filtered.csv')
-y = np.loadtxt('./data/processed/y_filtered.csv')
+os.chdir('/Users/alexanderjambor/Desktop/UCSD/SP23/BENG203/GroupProject/BRCAClassifier')
 
+X = np.loadtxt('./data/processed/X_filtered.csv', delimiter=',')
+y = np.loadtxt('./data/processed/y_filtered.csv', delimiter=',')
 
+model = LogisticRegression(solver='saga', penalty='elasticnet', l1_ratio=0.5)
+
+auroc_vals = []
+auprc_vals = []
+accuracy_scores = []
+f1_scores = []
+
+fpr_vecs = []
+tpr_vecs = []
+precision_vecs = []
+recall_vecs = []
+
+cv = StratifiedKFold(n_splits=5)
+thresholds = np.linspace(0, 1, 1000000)
+
+for train_idx, test_idx in cv.split(X, y):
+    X_train, X_test = X[train_idx], X[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
+
+    model.fit(X_train, y_train)
+    y_proba = model.predict_proba(X_test)[:, 1]
+    y_pred = model.predict(X_test)
+
+    fpr, tpr, _ = roc_curve(y_test, y_proba)
+    fpr_vecs.append(fpr)
+    tpr_vecs.append(tpr)
+    auroc = auc(fpr, tpr)
+    auroc_vals += [auroc]
+
+    precision, recall, _ = precision_recall_curve(y_test, y_proba)
+
+    precision_vecs.append(precision)
+    recall_vecs.append(recall)
+    auprc = auc(recall, precision)
+    auprc_vals += [auprc]
+
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_scores.append(accuracy)
+    f1 = f1_score(y_test, y_pred)
+    f1_scores.append(f1)
+
+avg_accuracy = sum(accuracy_scores) / len(accuracy_scores)
+avg_f1 = sum(f1_scores) / len(f1_scores)
+avg_auroc = sum(auroc_vals) / len(auroc_vals)
+avg_auprc = sum(auprc_vals) / len(auprc_vals)
+
+print("<Accuracy>:", avg_accuracy)
+print("<F1 Score>:", avg_f1)
+print("<AUROC>:", avg_auroc)
+print("<AUPRC>:", avg_auprc)
+
+plt.subplot(1, 2, 1)
+for idx, _ in enumerate(recall_vecs):
+    plt.plot(recall_vecs[idx], precision_vecs[idx])
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+
+plt.subplot(1, 2, 2)
+for idx, _ in enumerate(fpr_vecs):
+    plt.plot(fpr_vecs[idx], tpr_vecs[idx])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+
+plt.legend()
+plt.show()
